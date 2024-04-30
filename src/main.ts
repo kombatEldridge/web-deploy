8176107cb8b20dcd4f359c3022fef8332e89c4f2
Brinton Eldridge <90061933+kombatEldridge@users.noreply.github.com>
2024-04-30 15:49:55 -0500

    Update main.ts
    added -o HostKeyAlgorithms=+ssh-rsa

--- src/main.ts
+++ src/main.ts
@@ -63,7 +63,7 @@ export async function syncFiles(privateKeyPath: string, args: IActionArguments) 
   try {
     const rsyncArguments: string[] = [];
 
-    rsyncArguments.push(...stringArgv(`-e 'ssh -p ${args.ssh_port} -i ${privateKeyPath} -o StrictHostKeyChecking=no'`));
+    rsyncArguments.push(...stringArgv(`-e 'ssh -oHostKeyAlgorithms=+ssh-rsa  -p ${args.ssh_port} -i ${privateKeyPath} -o StrictHostKeyChecking=no'`));
 
     rsyncArguments.push(...stringArgv(args.rsync_options));
 
